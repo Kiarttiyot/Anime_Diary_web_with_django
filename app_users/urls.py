e28c9fb293e5a3c097c1a35a3df2f53f34344190
KiarttiyotChanpheng <66200028@kmitl.ac.th>
2025-08-09 21:23:43 +0700

    add google_login template

--- app_users/urls.py
+++ app_users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from allauth.account.views import LoginView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='account/loging.html'), name='account_login'),
+    path('login/', LoginView.as_view(template_name='account/login.html'), name='account_login'),
    
-    
 ]
