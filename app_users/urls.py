2ee238d516f6341a09fab5fe4c2e0943bd1e9195
KiarttiyotChanpheng <66200028@kmitl.ac.th>
2025-08-14 12:10:50 +0700

    dashboard/

--- app_users/urls.py
+++ app_users/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
-    path('dashboard',view=views.dashboard,name='dashboard'),
+    path('dashboard/',view=views.dashboard,name='dashboard'),
 ]
