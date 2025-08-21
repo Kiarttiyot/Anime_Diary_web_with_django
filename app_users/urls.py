9cee16e6c45f750fb6db7ee634241f44bac5a44f
chanasorn <Chanasorn1013@gmail.com>
2025-08-22 04:13:49 +0700

    dashboard-user

--- app_users/urls.py
+++ app_users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
     path('dashboard/',view=views.dashboard,name='dashboard'),
     path('profile/',view=views.profile, name='profile'),
+    path("search-users/", views.search_user, name="search_user"),  
+    path("dashboard/<str:username>/", views.dashboard_view, name="dashboard"),
+
 ]
--- app_users/views.py
+++ app_users/views.py
@@ -4,10 +4,28 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from app_users.forms import UserProfileForm,ExtendedProfileForm
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render
 
 @login_required
 def dashboard(request: HttpRequest):
     return render(request,"account/dashboard.html")
+
+def search_user(request):
+    q = request.GET.get("q")
+    if q:
+        try:
+            user_obj = User.objects.get(username=q)
+            return redirect("dashboard", username=user_obj.username)
+        except User.DoesNotExist:
+            # ส่ง error message กลับไปหน้า home (หรือหน้าเดิม)
+            return render(request, "appgenral/home.html", {"error": "User not found"})
+    return redirect("home")  # ถ้าไม่ได้พิมพ์อะไรเลย
+
+def dashboard_view(request, username):
+    # เอา user ที่เราต้องการแสดง ไม่ใช่ request.user
+    user_profile = get_object_or_404(User, username=username)
+    return render(request, "account/dashboard.html", {"user_profile": user_profile})
 
 @login_required
 def profile(request:HttpRequest):
