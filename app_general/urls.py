7b379568617a4b9430560835cdaea9d44457c03e
KiarttiyotChanpheng <66200028@kmitl.ac.th>
2025-09-05 18:13:37 +0700

    search success version 1

--- app_general/urls.py
+++ app_general/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 from app_users import views as user_views
 
+
 urlpatterns = [
     path('', views.home,name='home'),
     path('about/',views.about,name='about'),
     path('about_us/', views.about_us, name='about_us'),
     path('add_comment/<int:post_id>/', user_views.add_comment, name='add_comment'),
-    path('')
-
+    path('friend/', views.friend, name='friend'),
 ]
