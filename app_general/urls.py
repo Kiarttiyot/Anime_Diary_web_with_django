4a653c24f47d70805ace995e80d58aaa64855d65
KiarttiyotChanpheng <66200028@kmitl.ac.th>
2025-09-05 14:54:02 +0700

    friend template

--- app_general/urls.py
+++ app_general/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/',views.about,name='about'),
     path('about_us/', views.about_us, name='about_us'),
     path('add_comment/<int:post_id>/', user_views.add_comment, name='add_comment'),
+    path('')
 
 ]
